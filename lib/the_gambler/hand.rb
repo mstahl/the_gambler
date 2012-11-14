1cda690e9ed26c2ae6a013d62c6675cbda5efb2b
max thom stahl <max@villainousindustries.com>
2012-11-13 18:39:10 -0600

    Separated out blackjack-related functionality into its own concern.

--- lib/the_gambler/hand.rb
+++ lib/the_gambler/hand.rb
@@ -1,37 +1,12 @@
 module TheGambler
   class Hand
+    include TheGambler::Blackjack
+    
     attr_reader :contents
     
     def initialize(*args)
       @contents = []
       args.each{|a| @contents << Card.new(a) }
-    end
-    
-    def blackjack?
-      contents.count == 2 and contents.select(&:face_card?).count == 1 and contents.select(&:ace?).count == 1
-    end
-    
-    def blackjack_value
-      value = 0
-      # Add up all the values in the hand that aren't aces.
-      contents.reject(&:ace?).each do |card|
-        if card.face_card? then
-          value += 10
-        else
-          value += card.numerical_value
-        end
-      end
-      
-      # Now add up the aces greedily
-      contents.select(&:ace?).each do |card|
-        if 21 - value >= 11 then
-          value += 11
-        else
-          value += 1
-        end
-      end
-      
-      value
     end
     
     def poker_value
