6b0d33bc288b0bde67668008f6405d99203f42fe
max thom stahl <max@villainousindustries.com>
2012-11-17 21:28:39 -0600

    Four of a kind and three of a kind 100% fully working.

--- lib/the_gambler/poker.rb
+++ lib/the_gambler/poker.rb
@@ -8,41 +8,62 @@ module TheGambler
     
     def poker_value
       if royal_flush? then
+        
         10e10
+        
       elsif straight_flush? then
+        
         if contents.map(&:numerical_value).sort == [2, 3, 4, 5, 14] then
           10e9 + 1
         else
           10e9 + contents.max_by(&:numerical_value).numerical_value
         end
+        
       elsif four_of_a_kind? then
+        
         c = contents.group_by(&:numerical_value)
-        10e8 + c.keys.max_by{|k| c[k].count}
+        10e8 + 13 * c.keys.max_by{|k| c[k].count} + c.keys.min_by{|k| c[k].count}
+        
       elsif full_house? then
+        
         c = contents.group_by(&:numerical_value)
         10e7 + c.keys.detect{|k| c[k].count == 3} * 13 + c.keys.detect{|k| c[k].count == 2}
+        
       elsif flush? then
+        
         10e6 + contents.max_by(&:numerical_value).numerical_value
+        
       elsif straight? then
+        
         if contents.map(&:numerical_value).sort == [2, 3, 4, 5, 14] then
           10e5 + 1
         else
           10e5 + contents.max_by(&:numerical_value).numerical_value
         end
+        
       elsif three_of_a_kind? then
+        
         c = contents.group_by(&:numerical_value)
-        10e4 + c.keys.detect{|k| c[k].count == 3}
+        a, b = c.keys.select{|k| c[k].count == 1}.minmax
+        10e4 + 13 * 13 * c.keys.detect{|k| c[k].count == 3} + 13 * b + a
+        
       elsif two_pair? then
+        
         c = contents.group_by(&:numerical_value)
         
         pair_one, pair_two = c.keys.select{|k| c[k].count == 2}.minmax
         
         10e3 + 13 * pair_two + pair_one
+        
       elsif one_pair? then
+        
         c = contents.group_by(&:numerical_value)
         10e2 + c.keys.detect{|k| c[k].count == 2}
+        
       elsif high_card? then
+        
         contents.max_by(&:numerical_value).numerical_value
+        
       end
     end
     
